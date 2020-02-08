830638ed356bcdab976ab52bdf8a67c945bbc096
lavikoder <kevkamanda@gmail.com>
2020-02-08 18:52:44 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,51 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :items, :price, :quantity
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @quantity = quantity
+    @items = []
+  end
+
+
+
+  def add_item(title, price, quantity = 1)
+    @price = price
+    @quantity = quantity
+    @total += (price * quantity)
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << title
+        counter += 1
+      end
+    else
+      @items << title
+    end
+  end
+
+  def apply_discount
+    if @discount <= 0
+      return "There is no discount to apply."
+    elsif @discount > 0
+      puts "#{@total} :total amount"
+      puts "#{@discount} :discount"
+
+      amount_off = @total * (@discount.to_f / 100.to_f)
+      puts "#{amount_off} :amount off"
+      @total -= amount_off
+      return "After the discount, the total comes to $#{self.total.to_i}."
+    end
+  end
+
+  def void_last_transaction
+    #make quantity accessible for all methods by initializing it
+    price = @price * @quantity
+    totes = self.total
+    @total = totes - price
+
+    @items.pop()
+  end
+end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -41,18 +41,23 @@ describe 'CashRegister' do
   describe '#apply_discount' do
     context 'the cash register was initialized with an employee discount' do
       it 'applies the discount to the total price' do
+        puts "**** before test ****"
+        puts cash_register_with_discount.total
         cash_register_with_discount.add_item("macbook air", 1000)
+        puts "**** after add item ****"
+        puts cash_register_with_discount.total
         cash_register_with_discount.apply_discount
         expect(cash_register_with_discount.total).to eq(800)
       end
 
       it 'returns success message with updated total' do
         cash_register_with_discount.add_item("macbook air", 1000)
+        # cash_register_with_discount.total = 1000
         expect(cash_register_with_discount.apply_discount).to eq("After the discount, the total comes to $800.")
       end
 
       it 'reduces the total' do
-        cash_register.total = 0
+        # cash_register.total = 0
         cash_register_with_discount.add_item("macbook air", 1000)
         expect{cash_register_with_discount.apply_discount}.to change{cash_register_with_discount.total}.by(-200)
       end
@@ -78,8 +83,10 @@ describe 'CashRegister' do
     it 'subtracts the last item from the total' do
       cash_register.add_item("apple", 0.99)
       cash_register.add_item("tomato", 1.76)
+      puts cash_register.price
       cash_register.void_last_transaction
       expect(cash_register.total).to eq(0.99)
+      puts cash_register.total
     end
 
     it 'returns the total to 0.0 if all items have been removed' do
@@ -88,4 +95,3 @@ describe 'CashRegister' do
     end
   end
 end
-
